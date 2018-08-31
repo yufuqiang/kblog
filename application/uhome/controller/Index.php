747b43f70aa5e474e1712b45cfc73ec32ae9f785
yfq <619774356@qq.com>
2018-08-31 23:58:19 +0800

    add

--- application/uhome/controller/Index.php
+++ application/uhome/controller/Index.php
@@ -6,7 +6,7 @@ class Index
     public function index()
     {
         echo 2;
-        
+        Dump(1);
         $data = [["username"=>"11","password"=>"dads"],["username"=>"22","password"=>"dads"],["username"=>"33","password"=>"dads"]];
         $UserM = new User();
         $UserM->addAll($data);
